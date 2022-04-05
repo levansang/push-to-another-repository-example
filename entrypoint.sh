975b697c8a10d3aab4906f09921d882b4570a274
manhnh <manhnh@vmodev.com>
2022-04-05 17:15:40 +0700

    [main] testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,8 @@ echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 #git checkout -b $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git"
+# "$CLONE_DIR"
 echo 1
 
 if [ ! -z "$INPUT_RENAME" ]
